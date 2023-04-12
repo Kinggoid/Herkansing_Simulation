f4160c046048310555abcf73f5e86b1203ed30d1
Kinggoid <thijmespambox@gmail.com>
2023-04-12 10:15:00 +0200

    Made class "Coffee"

--- code/Domain/Coffee.py
+++ code/Domain/Coffee.py
@@ -1,5 +1,21 @@
 class Coffee:
-    def __init__(self, coffee, price):
-        self.coffee = coffee
-        self.price = price
+    made = 0
 
+    def __init__(self, coffee, price):
+        self._name = coffee
+        self._price = price
+
+    def name(self):
+        return self._name
+
+    def set_name(self, coffee):
+        self._name = coffee
+
+    def price(self):
+        return self._price
+
+    def set_price(self, price):
+        self._price = price
+
+    def __repr__(self):
+        return f"Coffee(name='{self._name}', price='{self._price}')"
---
+++ code/main.py
@@ -0,0 +1,6 @@
+print('hello project')
+
+from Domain.Coffee import Coffee
+
+a = Coffee("hello", 1)
+print(a)
