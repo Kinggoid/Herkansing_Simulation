65816b32218885a0272f4a3ca133077fd007dcd3
Kinggoid <thijmespambox@gmail.com>
2023-04-16 21:05:25 +0200

    User gains coffee

--- code/simulation/setSimulation.py
+++ code/simulation/setSimulation.py
@@ -6,7 +6,8 @@ class StatesHandler:
         self.machine = machine
         self.user = user
 
-    def handle(self, state, cargo):
+    def handler(self, state, cargo):
+        """Correctly assigns the state to the specific state handler."""
         if state == "q0":
             return self.q0(cargo[0], cargo[1:])
         elif state == "q1":
@@ -19,15 +20,13 @@ class StatesHandler:
             return self.q4(cargo)
         elif state == "q5":
             return self.q5(cargo[0], cargo[1:])
-        else:
-            raise ValueError("This command doesn't work.")
 
     def q0(self, command, cargo):
         if command == "TURN_ON":
             self.machine.turn_on()
             return ["q1", cargo]
         else:
-            raise ValueError("This command doesn't work.")
+            raise Exception("First turn the coffeemachine on.")
 
     def q1(self, command, cargo):
         if command == "PAY":
@@ -35,11 +34,11 @@ class StatesHandler:
             try:
                 money = float(money)
             except:
-                raise ValueError("This command: '" + money + "' doesn't correctly specify money.")
+                raise Exception("This command: '" + money + "' doesn't correctly specify the amount of money.")
             if self.user.lose_cash(money):
                 self.machine.pay_cash(money)
                 return ["q1", cargo[1:]]
-            raise ValueError("User doesn't have enough money to pay this amount.")
+            raise Exception("User doesn't have enough money to pay this amount.")
         elif command == "CHANGE":
             self.user.gain_cash(self.machine.pay_back_change())
             return "q1", cargo
@@ -48,7 +47,7 @@ class StatesHandler:
             self.machine.hs.choose_coffee(coffee)
             return "q2", cargo[1:]
         else:
-            raise ValueError("This command doesn't work.")
+            raise Exception("This command doesn't work.")
 
     def q2(self, command, cargo):
         if command == "PAY":
@@ -56,11 +55,11 @@ class StatesHandler:
             try:
                 money = float(money)
             except:
-                raise ValueError("This command: '" + money + "' doesn't correctly specify money.")
+                raise Exception("This command: '" + money + "' doesn't correctly specify the amount of money.")
             if self.user.lose_cash(money):
                 self.machine.pay_cash(money)
                 return ["q2", cargo[1:]]
-            raise ValueError("User doesn't have enough money to pay this amount.")
+            raise Exception("User doesn't have enough money to pay this amount.")
         elif command == "CHANGE":
             self.user.gain_cash(self.machine.pay_back_change())
             return "q2", cargo
@@ -70,7 +69,7 @@ class StatesHandler:
         elif command == "BUY":
             return "q3", cargo
         else:
-            raise ValueError("This command doesn't work.")
+            raise Exception("This command doesn't work.")
 
     def q3(self, cargo):
         if self.machine.hs.chosen_coffee.pay_coffee():
@@ -79,7 +78,9 @@ class StatesHandler:
             return "q2", cargo
 
     def q4(self, cargo):
-        self.machine.make_coffee(self.machine.hs.chosen_coffee.coffee.get_name())
+        chosen_coffee_name = self.machine.hs.chosen_coffee.coffee.get_name()
+        made_coffee = self.machine.make_coffee(chosen_coffee_name)
+        self.user.gain_coffee(made_coffee)
         return "q5", cargo
 
     def q5(self, command, cargo):
@@ -89,4 +90,4 @@ class StatesHandler:
         elif command == "STOP":
             return "END", None
         else:
-            raise ValueError("This command doesn't work.")
+            raise Exception("This command doesn't work.")
